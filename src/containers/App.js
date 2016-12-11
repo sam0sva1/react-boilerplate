7b901d4413bae11249a0320d2198e7df33c7c04e
sam0sva1 <konstantus@live.ru>
2016-12-12 00:04:28 +0300

    Added file loader

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,11 @@ import './app.css'
 
 export default class App extends Component {
 	render() {
-		return <div className='app'>Привет из App</div>
+		return (
+			<div className='app'>
+				<div>Darth Vader welcomes you!</div>
+				<img src='../static/dv.png' alt='logo' />
+			</div>
+		)
 	}
 }
